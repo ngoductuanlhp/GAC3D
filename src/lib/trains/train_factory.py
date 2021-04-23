070afe9a94bcebb0c1bfd8dd65667616494ecf49
quan5609 <quan.buicompscibk@hcmut.edu.vn>
2021-04-23 10:27:11 +0700

    colab code

--- src/lib/trains/train_factory.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from .car_pose import CarPoseTrainer
-train_factory = {
-  'car_pose': CarPoseTrainer,
-}
